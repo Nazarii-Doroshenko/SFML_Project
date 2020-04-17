91dea3a8c9a61168715be0dd9901e3384907cb20
Nazarii-Doroshenko <nazarii.doroshenko@gmail.com>
2020-04-17 13:42:04 +0300

    clean

--- Program.cs
+++ Program.cs
@@ -12,15 +12,18 @@ namespace NEWTEST
         static RenderWindow window, ControlsWindow;
         static bool IsWindowOpen() => window.IsOpen;
         static bool IsHelpWindowOpen() => ControlsWindow.IsOpen;
+
         static CircleShape circle;
         static RectangleShape rect, rect1, rect2, rectb1, recty1, recty2;
-        static RectangleShape rectbl1, rectbl2;//bl = block
+        static RectangleShape rectbl1, rectbl2;
+
         static Font Arial = new Font(@"9041.ttf");
+
         static int R = 40;
 
-        static byte red = 20;
-        static byte green = 20;
-        static byte blue = 20;
+        static byte red = 140;
+        static byte green = 140;
+        static byte blue = 140;
 
 
 
@@ -79,14 +82,48 @@ namespace NEWTEST
                 {
                     ControlsWindow.Close();
                 }
-                if (Keyboard.IsKeyPressed(Keyboard.Key.F) || Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
                 {
                     ControlsWindow.Close();
                 }
 
             }
         }
+        static void TextInGame()
+        {
+            string help = "press 'h' for help";
+            Text Thelp = new Text(help, Arial, 50);
+            Thelp.Position = new Vector2f(830, 15);
+            Thelp.DisplayedString = help;
+            window.Draw(Thelp);
 
+        }
+        static void Game()
+        {
+
+
+            window = new RenderWindow(new VideoMode(1200, 800), "TEST SFML_ PROJECT");
+
+
+            window.Clear(Color.Black);
+            window.SetFramerateLimit(60);
+            ShapesRend();
+
+
+
+            while (IsWindowOpen())
+            {
+
+
+                window.DispatchEvents();
+                FillColor();
+
+                Draw();
+
+
+            }
+
+        }
         static void ShapesDraw()
         {
 
@@ -103,6 +140,10 @@ namespace NEWTEST
             rectb1.Draw(window, RenderStates.Default);
 
             circle.Draw(window, RenderStates.Default);
+
+
+
+            TextInGame();
 
 
         }
@@ -174,32 +215,15 @@ namespace NEWTEST
 
             rectbl1 = new RectangleShape(new Vector2f(110, 90));
         }
+
+
+
         static void Render()
         {
-
-            window = new RenderWindow(new VideoMode(1200, 800), "TEST SFML_ PROJECT");
             text();
-            while (IsHelpWindowOpen())
-            {
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Enter))
-                {
-                    ControlsWindow.Close();
-                }
+            Game();
 
-            }
-            ShapesRend();
 
-            // circle.Position = new Vector2f(100f, 100f);
-            window.SetFramerateLimit(45);
-            while (IsWindowOpen())
-            {
-
-                window.DispatchEvents();
-                FillColor();
-
-                Draw();
-
-            }
         }
         static void Input()
         {
@@ -229,7 +253,6 @@ namespace NEWTEST
                 ApplyAdditionalMoveModifeirs(dx, dy);
 
             }
-            logic(dx, dy);
             if (Keyboard.IsKeyPressed(Keyboard.Key.X))
             {
                 R++;
@@ -311,7 +334,7 @@ namespace NEWTEST
             }
             circle.FillColor = new Color(red, green, blue);
         }
-        
+
         static void LControl(float dx, float dy)
         {
             dx /= 2;
@@ -344,47 +367,6 @@ namespace NEWTEST
             {
                 DefaultGo(dx, dy);
             }
-
-
-        }
-        static void logic(float dx, float dy)
-        {
-
-
-
-            if (circle.Position == new Vector2f(1200f, 90f))
-            {
-                circle.Position = new Vector2f(0f, 0f);
-            }
-            else if (circle.Position == new Vector2f(110, 90))
-            {
-                circle.Position = new Vector2f(0f, 0f);
-            }
-            else if (circle.Position == new Vector2f(10f, 800f))
-            {
-                circle.Position = new Vector2f(0f, 0f);
-            }
-            else if (circle.Position == new Vector2f(1190f, 790f))
-            {
-                circle.Position = new Vector2f(0f, 0f);
-            }
-            else if (circle.Position == new Vector2f(1200f, 10f))
-            {
-                circle.Position = new Vector2f(0f, 0f);
-            }
-            /* else if (circle.Position == new Vector2f(0.5f, 100f))
-             {
-                 circle.Position = new Vector2f(0f, 0f);
-             }
-             else if (circle.Position == new Vector2f(100f, 0.5f))
-             {
-                 circle.Position = new Vector2f(0f, 0f);
-             }*/
-            else
-            {
-                //circle.Position = new Vector2f(dx, dy);
-            }
-
         }
 
 
